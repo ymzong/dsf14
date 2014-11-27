04b366a1dc21b14857eaec44a0c00d8bdc609d68
Jimmy Zong <yzong@cmu.edu>
2014-11-27 03:33:23 -0500

    Completed tentative parallel 2D K-means.

--- OpenMPIKMeans/src/com/yzong/dsf14/openMPIKMeans/euclidean/EuclideanParallel.java
+++ OpenMPIKMeans/src/com/yzong/dsf14/openMPIKMeans/euclidean/EuclideanParallel.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Set;
 
 import mpi.MPI;
+import mpi.Status;
 
 /**
  * Entry point for parallel K-Means algorithm in 2D Euclidean space.
@@ -20,6 +21,31 @@ import mpi.MPI;
 public class EuclideanParallel {
 
   private final static int MASTER_ID = 0;
+  private final static int CLUSTERX_PKG = 15440;
+  private final static int CLUSTERY_PKG = 15441;
+  private final static int NUMD_PKG = 15442;
+  private final static int ASSOC_PKG = 15443;
+  private final static int XTOTAL_PKG = 15444;
+  private final static int YTOTAL_PKG = 15445;
+  private final static int CONT_PKG = 15446;
+
+  private String InputFile;
+  private double Epsilon;
+  private double X[];
+  private double Y[];
+  private double MXold[];
+  private double MYold[];
+  private double MX[];
+  private double MY[];
+  private double XTotal[];
+  private double YTotal[];
+  private int N;
+  private int K;
+
+  private int MyRank;
+  private int UnivSize;
+  private int StartIdx;
+  private int EndIdx;
 
   /**
    * Main function for parallel K-Means for 2D points.
@@ -27,24 +53,233 @@ public class EuclideanParallel {
    * @param args <tt>InputFileName</tt>, <tt>NumberOfClusters</tt>, <tt>TerminationThreshold</tt>.
    */
   public static void main(String[] args) {
-    /* Initialization phase. */
-    String InputFile = "";
-    double Epsilon = 0;
-    int K = 0;
-    List<Double> xList = new ArrayList<Double>();
-    List<Double> yList = new ArrayList<Double>();
-    /* Parse command-line arguments. */
+    MPI.Init(args);
+    EuclideanParallel ep = new EuclideanParallel();
+    ep.run(args);
+    MPI.Finalize();
+    return;
+  }
+
+  public void run(String[] args) {
+    /* All: Load MPI-related constants. */
+    MyRank = MPI.COMM_WORLD.Rank();
+    UnivSize = MPI.COMM_WORLD.Size();
+    if (UnivSize < 2) {
+      if (MyRank == MASTER_ID) {
+        System.out.println("Only one MPI instance found! Use sequential version instead.");
+      }
+      MPI.Finalize();
+      System.exit(1);
+    }
+    /* All: Load input parameters and data from user. */
+    extractInput(args);
+    int chunkSize = (int) Math.ceil(((double) N) / (UnivSize - 1));
+    StartIdx = chunkSize * MyRank;
+    EndIdx = Math.min(chunkSize * MyRank + chunkSize, N);
+
+    /* All: Rule out trivial case. */
+    if (K > N) {
+      if (MyRank == MASTER_ID) {
+        System.out.println("Number of clusters greater than data size!");
+        System.out.println("Each cluster consists of one data entry; remaining clusters empty.");
+      }
+      MPI.Finalize();
+      System.exit(0);
+    }
+
+    /* All: Initialize arrays. */
+    MXold = new double[K];
+    MYold = new double[K];
+    MX = new double[K];
+    MY = new double[K];
+    /* Master: Pick K random points as initial ctrs. */
+    if (MyRank == MASTER_ID) {
+      initCtrs();
+    }
+
+    /* All: Start K-Means iterations. */
+    int numD[]; // Number of elements associated with each center
+    int assoc[]; // Center association of each element
+    while (true) {
+      /* Broadcast all MXold, MYold cluster coordinates. */
+      broadcastMXYold();
+      /* Initialize data structures for calculation. */
+      numD = new int[K];
+      assoc = new int[N];
+      XTotal = new double[K];
+      YTotal = new double[K];
+      /* Worker: Re-associate nodes in the interval [StartIdx, EndIdx). */
+      if (MyRank != MASTER_ID) {
+        /* Calculate new associations. */
+        for (int elem = StartIdx; elem < EndIdx; elem++) {
+          double minDist = Double.MAX_VALUE;
+          int minIndex = -1;
+          for (int cluster = 0; cluster < K; cluster++) {
+            double d =
+                Math.sqrt(Math.pow(X[elem] - MXold[cluster], 2)
+                    + Math.pow(Y[elem] - MYold[cluster], 2));
+            if (d < minDist) {
+              minDist = d;
+              minIndex = cluster;
+            }
+          }
+          numD[minIndex]++;
+          assoc[elem] = minIndex;
+          XTotal[minIndex] += X[elem];
+          YTotal[minIndex] += Y[elem];
+        }
+        /* Send result to Master */
+        MPI.COMM_WORLD.Send(numD, 0, K, MPI.INT, MASTER_ID, NUMD_PKG);
+        MPI.COMM_WORLD.Send(assoc, StartIdx, EndIdx - StartIdx, MPI.INT, MASTER_ID, ASSOC_PKG);
+        MPI.COMM_WORLD.Send(XTotal, 0, K, MPI.DOUBLE, MASTER_ID, XTOTAL_PKG);
+        MPI.COMM_WORLD.Send(YTotal, 0, K, MPI.DOUBLE, MASTER_ID, YTOTAL_PKG);
+        /* Block till Master informs convergence or not. */
+        boolean convg = false;
+        MPI.COMM_WORLD.Recv(convg, 0, 1, MPI.BOOLEAN, MASTER_ID, CONT_PKG);
+        if (convg) {
+          break; // If Master finds convergence, Worker instance breaks out of while loop.
+        }
+      }
+      /* Master: Gather the results from Workers. */
+      else {
+        double doubleBuffer[] = new double[K];
+        int intBuffer[] = new int[Math.max(chunkSize, K)];
+        for (int count = 1; count < UnivSize; count++) {
+          // Process numD arrays.
+          MPI.COMM_WORLD.Recv(intBuffer, 0, K, MPI.INT, MPI.ANY_SOURCE, NUMD_PKG);
+          for (int cluster = 0; cluster < K; cluster++) {
+            numD[cluster] += intBuffer[cluster];
+          }
+          // Process XTotal and YTotal arrays.
+          MPI.COMM_WORLD.Recv(doubleBuffer, 0, K, MPI.DOUBLE, MPI.ANY_SOURCE, XTOTAL_PKG);
+          for (int cluster = 0; cluster < K; cluster++) {
+            XTotal[cluster] += doubleBuffer[cluster];
+          }
+          MPI.COMM_WORLD.Recv(doubleBuffer, 0, K, MPI.DOUBLE, MPI.ANY_SOURCE, YTOTAL_PKG);
+          for (int cluster = 0; cluster < K; cluster++) {
+            YTotal[cluster] += doubleBuffer[cluster];
+          }
+          // Stick up associations in assoc sub-arrays.
+          Status s =
+              MPI.COMM_WORLD.Recv(intBuffer, 0, chunkSize, MPI.INT, MPI.ANY_SOURCE, NUMD_PKG);
+          int start = chunkSize * s.source;
+          int end = Math.min(chunkSize * s.source + chunkSize, N);
+          for (int elem = start; elem < end; elem++) {
+            numD[elem] = intBuffer[elem - start];
+          }
+        }
+        /* Re-calculate center coordinates. */
+        for (int cluster = 0; cluster < K; cluster++) {
+          /* Rare case: no data points associated with cluster. Center set to random point. */
+          if (numD[cluster] == 0) {
+            int indx = (int) (Math.random() * N);
+            MX[cluster] = X[indx];
+            MY[cluster] = Y[indx];
+            System.out.printf("Warning: no points assigned to cluster %d!\n", cluster);
+          } else {
+            MX[cluster] = XTotal[cluster] / numD[cluster];
+            MY[cluster] = YTotal[cluster] / numD[cluster];
+          }
+        }
+        /* Check if centers converge. */
+        boolean converge = true;
+        for (int i = 0; i < K; i++) {
+          if (Math.sqrt(Math.pow(MX[i] - MXold[i], 2) + Math.pow(MY[i] - MYold[i], 2)) > Epsilon) {
+            converge = false;
+            break;
+          }
+        }
+        /* Tell children to about convergence. */
+        for (int rank = 1; rank < UnivSize; rank++) {
+          MPI.COMM_WORLD.Send(converge, 0, 1, MPI.BOOLEAN, rank, CONT_PKG);
+        }
+        /* If all centers converge, complete! */
+        if (converge) {
+          break;
+        }
+        /* Otherwise, update center coordinates. */
+        else {
+          MXold = MX.clone();
+          MYold = MY.clone();
+        }
+      }
+    }
+    /* Writes result to local file. */
+    if (MyRank == MASTER_ID) {
+      outputResult(assoc);
+    }
+    return;
+  }
+
+  /**
+   * Outputs final result to local file. (Used by Master)
+   * 
+   * @param assoc Final association of each data entry with its cluster
+   */
+  private void outputResult(int[] assoc) {
+    try {
+      String OutputFile = InputFile + ".out";
+      System.out.printf("Writing clustering result to `%s`...", OutputFile);
+      PrintWriter writer = new PrintWriter(OutputFile, "UTF-8");
+      writer.println("Following are the cluster centers:");
+      for (int cluster = 0; cluster < K; cluster++) {
+        writer.printf("Cluster %d: %f, %f\n", cluster, MX[cluster], MY[cluster]);
+      }
+      writer.println("Following are the cluster association for each element:");
+      for (int elem = 0; elem < N; elem++) {
+        writer.printf("%d\n", assoc[elem]);
+      }
+      writer.println("--- End of Output ---");
+      writer.flush();
+      writer.close();
+      System.out.printf("Done!\n");
+    } catch (IOException e) {
+      System.out.printf("\nFailed to write to output file -- %s\n", e.getMessage());
+      return;
+    }
+  }
+
+  /**
+   * Broadcasts current value of MXold and MYold from master. (Used by All)
+   */
+  private void broadcastMXYold() {
+    if (MyRank == MASTER_ID) {
+      for (int rank = 1; rank < UnivSize; rank++) {
+        MPI.COMM_WORLD.Send(MXold, 0, K, MPI.DOUBLE, rank, CLUSTERX_PKG);
+        MPI.COMM_WORLD.Send(MYold, 0, K, MPI.DOUBLE, rank, CLUSTERY_PKG);
+      }
+    }
+    /* Workers: Receive initial centers from Master. */
+    else {
+      MPI.COMM_WORLD.Recv(MXold, 0, K, MPI.DOUBLE, MASTER_ID, CLUSTERX_PKG);
+      MPI.COMM_WORLD.Recv(MYold, 0, K, MPI.DOUBLE, MASTER_ID, CLUSTERY_PKG);
+    }
+  }
+
+  /**
+   * Loads input parameters and data from user. (Used by All)
+   * 
+   * @param args Command-line arguments from user
+   */
+  private void extractInput(String[] args) {
+    /* All: Parse command-line arguments. */
     try {
       InputFile = args[0];
       K = Integer.parseInt(args[1]);
       Epsilon = Double.parseDouble(args[2]);
     } catch (Exception e) {
-      System.out.printf("Usage: java com.yzong.dsf14.openMPIKMeans.euclidean.EuclideanSequential ");
-      System.out.printf("<InputFile> <#Clusters> <Threshold>\n");
+      /* If parameters invalid, Master prints out message. All quit. */
+      if (MyRank == MASTER_ID) {
+        System.out.printf("Usage: java com.yzong.dsf14.openMPIKMeans.euclidean");
+        System.out.printf(".EuclideanSequential <InputFile> <#Clusters> <Threshold>\n");
+      }
+      MPI.Finalize();
       System.exit(1);
     }
 
-    /* Load dataset from input file. */
+    /* All: Load dataset from input file. */
+    List<Double> xList = new ArrayList<Double>();
+    List<Double> yList = new ArrayList<Double>();
     BufferedReader in;
     try {
       in = new BufferedReader(new FileReader(InputFile));
@@ -55,166 +290,38 @@ public class EuclideanParallel {
       }
       in.close();
     } catch (IOException e) {
-      System.out.printf("Exception while reading input file -- %s\n", e.getMessage());
+      /* Should I/O Exception occur, Master prints out message. All quit. */
+      if (MyRank == MASTER_ID) {
+        System.out.printf("Exception while reading input file -- %s\n", e.getMessage());
+      }
+      MPI.Finalize();
       System.exit(1);
     }
-    /* Initialize global data structures. */
-    int N = xList.size();
-    double X[] = new double[N];
-    double Y[] = new double[N];
-    double MXold[] = new double[K];
-    double MYold[] = new double[K];
-    double MX[] = new double[K];
-    double MY[] = new double[K];
+    N = xList.size();
+    X = new double[N];
+    Y = new double[N];
     for (int i = 0; i < N; i++) {
       X[i] = xList.get(i);
-      Y[i] = xList.get(i);
+      Y[i] = yList.get(i);
     }
-    System.out.printf("Successfully loaded %d data points!\n", xList.size());
-    
-    /* Rule out trivial case. */
-    if (K > N) {
-      System.out.println("Number of clusters greater than data size!");
-      System.out.println("Each cluster consists of one data entry; remaining clusters empty.");
-      System.exit(0);
+    if (MyRank == MASTER_ID) {
+      System.out.printf("Successfully loaded %d data points!\n", xList.size());
     }
+  }
 
-    /* Initialize MPI session. */
-    MPI.Init(args);
-    int myRank = MPI.COMM_WORLD.Rank();
-    int totalSize = MPI.COMM_WORLD.Size();
-    // Master prints out success message.
-    if (myRank == MASTER_ID) {
-      System.out.println("MPI Session initialized!");
+  /**
+   * Initializes centers with random data entries. (Used by Master)
+   */
+  private void initCtrs() {
+    Set<Integer> subsetIdx = new HashSet<Integer>();
+    while (subsetIdx.size() < K) {
+      subsetIdx.add((int) (Math.random() * N));
     }
-    /* Master: Pick K random points as initial centers. Initialize arrays. */
-    if (myRank == MASTER_ID) {
-      Set<Integer> subsetIdx = new HashSet<Integer>();
-      while (subsetIdx.size() < K) {
-        subsetIdx.add((int) (Math.random() * N));
-      }
-      int counter = 0;
-      for (int s : subsetIdx) {
-        MXold[counter] = X[s];
-        MYold[counter] = X[s];
-        counter++;
-      }
-      /* Send MXold[] and MYold[] to each worker. */
-      for (int i = 1; i < totalSize; i++) {
-        MPI.COMM_WORLD.Send(MXold, 0, K, MPI.DOUBLE, i, 15440);
-        MPI.COMM_WORLD.Send(MYold, 0, K, MPI.DOUBLE, i, 15440);
-      }
-    } else {
-      /* Receives MXold[] and MYold[] from master. Blocks until arrives. */
-      MPI.COMM_WORLD.Recv(MXold, 0, K, MPI.DOUBLE, 0, 15440);
-      MPI.COMM_WORLD.Recv(MYold, 0, K, MPI.DOUBLE, 0, 15440);
+    int counter = 0;
+    for (int s : subsetIdx) {
+      MXold[counter] = X[s];
+      MYold[counter] = Y[s];
+      counter++;
     }
-
-    /*
-     * Parallel section starts. Worker receives MXold[], MYold[], and a portion of X[] and Y[].
-     * Master summarizes all worker results.
-     */
-    /* Start K-Means iterations. */
-    boolean B[][] = new boolean[K][N]; // True iff Element n is associated with Center k
-    double D[][] = new double[K][N]; // Distance between Element n and Center k
-    while (true) {
-      if (myRank != MASTER_ID) {
-        /* Calculate Euclidean distances. */
-        for (int cluster = 0; cluster < K; cluster++) {
-          // Parallelizable
-          for (int elem = 0; elem < N; elem++) {
-            D[cluster][elem] =
-                Math.sqrt(Math.pow(X[elem] - MXold[cluster], 2)
-                    + Math.pow(Y[elem] - MYold[cluster], 2));
-          }
-        }
-        /* Re-associate elements with centers. */
-        // Parallelizable
-        for (int elem = 0; elem < N; elem++) {
-          double minDist = Double.MAX_VALUE;
-          int minIndex = -1;
-          for (int cluster = 0; cluster < K; cluster++) {
-            if (D[cluster][elem] < minDist) {
-              minDist = D[cluster][elem];
-              minIndex = cluster;
-            }
-          }
-          for (int cluster = 0; cluster < K; cluster++) {
-            B[cluster][elem] = (cluster == minIndex);
-          }
-        }
-        /* Re-calculate center coordinates. */
-        for (int cluster = 0; cluster < K; cluster++) {
-          // Parallelizable
-          int elemCount = 0;
-          double XTotal = 0;
-          double YTotal = 0;
-          for (int elem = 0; elem < N; elem++) {
-            if (B[cluster][elem]) {
-              elemCount++;
-              XTotal += X[elem];
-              YTotal += Y[elem];
-            }
-          }
-          MX[cluster] = XTotal / elemCount;
-          MY[cluster] = YTotal / elemCount;
-          MXold = new double[K];
-          MYold = new double[K];
-        }
-      }
-
-      // TODO: Master reduces MX, MY from workers ==> MX[] and MY[].
-
-      /* Master: Check if centers converge. */
-      if (myRank == 0) {
-        boolean finish = true;
-        for (int i = 0; i < K; i++) {
-          if (Math.sqrt(Math.pow(MX[i] - MXold[i], 2) + Math.pow(MY[i] - MYold[i], 2)) > Epsilon) {
-            finish = false;
-            break;
-          }
-        }
-        /* If all centers converge, complete! */
-        if (finish) {
-          break;
-        }
-      }
-      /* Update center coordinates. */
-      MXold = MX.clone();
-      MYold = MY.clone();
-    }
-
-    //TODO: Master reduces results from workers.
-
-    /* Master: Output result to user */
-    if (myRank == 0) {
-      try {
-        String OutputFile = InputFile + ".out";
-        System.out.printf("Writing clustering result to `%s`...", OutputFile);
-        PrintWriter writer = new PrintWriter(OutputFile, "UTF-8");
-        writer.println("Following are the cluster centers:");
-        for (int cluster = 0; cluster < K; cluster++) {
-          writer.printf("Cluster %d: %f, %f\n", cluster, MX[cluster], MY[cluster]);
-        }
-        writer.println("Following are the cluster association for each element:");
-        for (int elem = 0; elem < N; elem++) {
-          for (int cluster = 0; cluster < K; cluster++) {
-            if (B[cluster][elem]) {
-              writer.printf("%d\n", cluster);
-            }
-          }
-        }
-        writer.println("--- End of Output ---");
-        writer.flush();
-        writer.close();
-        System.out.printf("Done!\n");
-      } catch (IOException e) {
-        System.out.printf("\nFailed to write to output file -- %s\n", e.getMessage());
-        MPI.Finalize();
-        System.exit(1);
-      }
-    }
-    /* Conclude the MPI session. */
-    MPI.Finalize();
   }
 }
